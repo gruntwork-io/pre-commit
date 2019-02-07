a6c8278495e64a0b231ed659341eee4b4c4e8eec
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-02-07 13:47:51 -0800

    Improvements based on review feedback

--- hooks/helmlint.sh
+++ hooks/helmlint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# We don't set `set -e` in this script because some of the functions will return a non-zero value (e.g contains_element)
+set -e
+
+# Run helm lint on the chart path.
+# helm lint can only be run on the root path of a chart, so this pre-commit hook will take the changed files and resolve
+# it to the helm chart path. The helm chart path is determined by a heuristic: it is the directory containing the
+# Chart.yaml file.
+# Note that pre-commit will only feed this the files that changed in the commit, so we can't do the filtering at the
+# hook setting level (e.g `files: Chart.yaml` will not work if no changes are made in the Chart.yaml file).
 
 # OSX GUI apps do not pick up environment variables the same way as Terminal apps and there are no easy solutions,
 # especially as Apple changes the GUI app behavior every release (see https://stackoverflow.com/q/135688/483528). As a
@@ -9,9 +16,6 @@ export PATH=$PATH:/usr/local/bin
 
 # Take the current working directory to know when to stop walking up the tree
 readonly cwd_abspath="$(realpath "$PWD")"
-
-# An array to keep track of which charts we already linted
-seen_chart_paths=()
 
 # https://stackoverflow.com/a/8574392
 # Usage: contains_element "val" "${array[@]}"
@@ -29,50 +33,49 @@ contains_element() {
 }
 
 # Only log debug statements if PRECOMMIT_DEBUG environment variable is set.
+# Log to stderr.
 debug() {
-  if [[ $PRECOMMIT_DEBUG ]]; then
-    echo "$@"
+  if [[ ! -z $PRECOMMIT_DEBUG ]]; then
+    >&2 echo "$@"
   fi
 }
 
 # Recursively walk up the tree until the current working directory and check if the changed file is part of a helm
 # chart. Helm charts have a Chart.yaml file.
-# Return value is stored in `chart_path_return`
-chart_path_return=""
 chart_path() {
-  # Return return value
-  chart_path_return=""
+  local -r changed_file="$1"
 
   # We check both the current dir as well as the parent dir, in case the current dir is a file
-  local -r changed_file="$(realpath "$1")"
-  local -r changed_file_dir="$(dirname "$changed_file")"
+  local -r changed_file_abspath="$(realpath "$changed_file")"
+  local -r changed_file_dir="$(dirname "$changed_file_abspath")"
 
-  debug "Checking directory $changed_file and $changed_file_dir for Chart.yaml"
+  debug "Checking directory $changed_file_abspath and $changed_file_dir for Chart.yaml"
 
   # Base case: we have walked to the top of dir tree
-  if [[ "$changed_file" == "$cwd_abspath" ]]; then
+  if [[ "$changed_file_abspath" == "$cwd_abspath" ]]; then
     debug "No chart path found"
+    echo ""
     return 0
   fi
 
   # The changed file is itself the helm chart indicator, Chart.yaml
-  if [[ "$(basename "$changed_file")" == "Chart.yaml" ]]; then
-    chart_path_return="$changed_file_dir"
-    debug "Chart path found: $chart_path_return"
+  if [[ "$(basename "$changed_file_abspath")" == "Chart.yaml" ]]; then
+    debug "Chart path found: $changed_file_dir"
+    echo "$changed_file_dir"
     return 0
   fi
 
   # The changed_file is the directory containing the helm chart package file
-  if [[ -f "$changed_file/Chart.yaml" ]]; then
-    chart_path_return="$changed_file"
-    debug "Chart path found: $chart_path_return"
+  if [[ -f "$changed_file_abspath/Chart.yaml" ]]; then
+    debug "Chart path found: $changed_file_abspath"
+    echo "$changed_file_abspath"
     return 0
   fi
 
   # The directory of changed_file is the directory containing the helm chart package file
   if [[ -f "$changed_file_dir/Chart.yaml" ]]; then
-    chart_path_return="$changed_file_dir"
-    debug "Chart path found: $chart_path_return"
+    debug "Chart path found: $changed_file_dir"
+    echo "$changed_file_dir"
     return 0
   fi
 
@@ -80,24 +83,20 @@ chart_path() {
   chart_path "$changed_file_dir"
 }
 
-# Check if the provided changed file is a chart path, and run helm lint if it is
-check_changed_file() {
-  chart_path "$1"
-  contains_element "$chart_path_return" "${seen_chart_paths[@]}"
-  if [[ $? -eq 0 ]]; then
-    return
-  fi
-
-  if [[ "$chart_path_return" != "" ]]; then
-    helm lint "$chart_path_return"
-    if [[ $? -ne 0 ]]; then
-      exit 1
-    fi
-  fi
-  seen_chart_paths+=( "$chart_path_return" )
-}
+# An array to keep track of which charts we already linted
+seen_chart_paths=()
 
 for file in "$@"; do
   debug "Checking $file"
-  check_changed_file "$file"
+  file_chart_path=$(chart_path "$file")
+  debug "Resolved $file to chart path $file_chart_path"
+
+  if [[ ! -z $file_chart_path ]]; then
+    if contains_element "$file_chart_path" "${seen_chart_paths[@]}"; then
+      debug "Already linted $file_chart_path"
+    else
+      helm lint "$file_chart_path"
+      seen_chart_paths+=( "$file_chart_path" )
+    fi
+  fi
 done
