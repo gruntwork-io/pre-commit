831ec4fe20d63f14e215b9294445df7bff79a0f8
Mark Butcher <macbutch@users.noreply.github.com>
2019-02-18 16:34:10 +1100

    Fix incorrect shebang detection

--- hooks/shellcheck.sh
+++ hooks/shellcheck.sh
@@ -10,7 +10,7 @@ export PATH=$PATH:/usr/local/bin
 exit_status=0
 
 for file in "$@"; do
- if (head -1 "$file" |grep '^#!.*[sh]'>/dev/null); then
+ if (head -1 "$file" |grep '^#!.*sh'>/dev/null); then
 
     if ! shellcheck "$file"; then
         exit_status=1
