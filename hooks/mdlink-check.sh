a2c5b338d858201367c4b0140df5d5dd990c377b
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-02-04 14:02:55 -0800

    Add precommit hook for running markdown-link-check

---
+++ hooks/mdlink-check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# OSX GUI apps do not pick up environment variables the same way as Terminal apps and there are no easy solutions,
+# especially as Apple changes the GUI app behavior every release (see https://stackoverflow.com/q/135688/483528). As a
+# workaround to allow GitHub Desktop to work, add this (hopefully harmless) setting here.
+export PATH=$PATH:/usr/local/bin
+
+TMP_CONFIG="$(mktemp)"
+cat > "$TMP_CONFIG" <<EOF
+{
+  "replacementPatterns": [
+    {
+      "pattern": "^/",
+      "replacement": "file://$(pwd)"
+    }
+  ]
+}
+EOF
+
+markdown-link-check -c "$TMP_CONFIG"
