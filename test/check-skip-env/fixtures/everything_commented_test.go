b7e6554cbdaca15adec931b6f49aa8458b7b742f
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-04-01 11:55:36 -0700

    Add unit tests for new python hook

---
+++ test/check-skip-env/fixtures/everything_commented_test.go
@@ -0,0 +1,35 @@
+package basecase
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/logger"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+)
+
+func TestWithStages(t *testing.T) {
+	t.Parallel()
+
+	// Uncomment the items below to skip certain parts of the test
+	//os.Setenv("TERRATEST_REGION", "eu-west-1")
+	//os.Setenv("SKIP_setup", "true")
+	//os.Setenv("SKIP_deploy", "true")
+	//os.Setenv("SKIP_validate", "true")
+	//os.Setenv("SKIP_cleanup", "true")
+
+	test_structure.RunTestStage(t, "setup", func() {
+		logger.Logf(t, "setup")
+	})
+
+	defer test_structure.RunTestStage(t, "cleanup", func() {
+		logger.Logf(t, "cleanup")
+	})
+
+	test_structure.RunTestStage(t, "deploy", func() {
+		logger.Logf(t, "deploy")
+	})
+
+	test_structure.RunTestStage(t, "validate", func() {
+		logger.Logf(t, "validate")
+	})
+}
