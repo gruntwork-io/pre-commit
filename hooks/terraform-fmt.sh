6547993836cc1b68e399ba4843bc6bc994a73296
tyron <tyron@users.noreply.github.com>
2021-08-09 14:43:40 -0400

    write changes to disk on terraform_fmt by default

--- hooks/terraform-fmt.sh
+++ hooks/terraform-fmt.sh
@@ -7,11 +7,43 @@ set -e
 # workaround to allow GitHub Desktop to work, add this (hopefully harmless) setting here.
 export PATH=$PATH:/usr/local/bin
 
+write_changes=true
+FILES=()
+
+parse_arguments() {
+	while (($# > 0)); do
+		# Grab param and value splitting on " " or "=" with parameter expansion
+		local PARAMETER="${1%[ =]*}"
+		local VALUE="${1#*[ =]}"
+		if [[ "$PARAMETER" == "$VALUE" ]]; then VALUE="$2"; fi
+		shift
+		case "$PARAMETER" in
+		--no-write)
+			write_changes=false
+			;;
+		-*)
+			echo "Error: Unknown option: $PARAMETER" >&2
+			exit 1
+			;;
+		*)
+			FILES+=("$PARAMETER")
+			;;
+		esac
+	done
+}
+
+parse_arguments "$@"
+
 # Store and return last failure from fmt so this can validate every directory passed before exiting
 FMT_ERROR=0
 
-for file in "$@"; do
-  terraform fmt -diff -check "$file" || FMT_ERROR=$?
+for file in "$FILES"; do
+  file=$(dirname "$file")
+  if [ "$write_changes" = true ]; then
+    terraform fmt "$file" || FMT_ERROR=$?
+  else
+    terraform fmt -diff -check "$file" || FMT_ERROR=$?
+  fi
 done
 
 exit ${FMT_ERROR}
