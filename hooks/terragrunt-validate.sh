372476521471e1ce0078a0e7ef96d93f05f2e520
Michael Barrientos <mike@phc.health>
2021-08-23 09:19:34 -0700

    Use terragrunt validate instead of terraform validate

--- hooks/terragrunt-validate.sh
+++ hooks/terragrunt-validate.sh
@@ -14,10 +14,10 @@ export TF_IN_AUTOMATION=1
 VALIDATE_ERROR=0
 
 for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
-  echo "--> Running 'terraform validate' in directory '$dir'"
+  echo "--> Running 'terragrunt validate' in directory '$dir'"
   pushd "$dir" >/dev/null
   terraform init -backend=false || VALIDATE_ERROR=$?
-  terraform validate || VALIDATE_ERROR=$?
+  terragrunt validate || VALIDATE_ERROR=$?
   popd >/dev/null
 done
 
