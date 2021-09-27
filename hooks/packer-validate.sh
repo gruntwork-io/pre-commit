2053191302eb02c4697235f87af56af6d0cb79ac
Andrew Graham <queglay@gmail.com>
2021-09-27 22:51:18 +0930

    Add packer validate (#58)

---
+++ hooks/packer-validate.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+# OSX GUI apps do not pick up environment variables the same way as Terminal apps and there are no easy solutions,
+# especially as Apple changes the GUI app behavior every release (see https://stackoverflow.com/q/135688/483528). As a
+# workaround to allow GitHub Desktop to work, add this (hopefully harmless) setting here.
+export PATH=$PATH:/usr/local/bin
+
+# Store and return last failure from validate so this can validate every directory passed before exiting
+VALIDATE_ERROR=0
+
+for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
+  echo "--> Running 'packer validate -syntax-only' in directory '$dir'"
+  pushd "$dir" >/dev/null
+  packer validate -syntax-only || VALIDATE_ERROR=$?
+  popd >/dev/null
+done
+
+exit ${VALIDATE_ERROR}
