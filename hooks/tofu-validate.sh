59fd8610ae21aaf8234f1ef17d43c3ccdee84d16
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2025-04-24 12:33:46 -0400

    feat: Adding tofu hooks (#129)
    * feat: Adding tofu hooks
    
    * feat: Updating README.md
    
    * fix: Updating `.pre-commit-hooks.yaml`

---
+++ hooks/tofu-validate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+# OSX GUI apps do not pick up environment variables the same way as Terminal apps and there are no easy solutions,
+# especially as Apple changes the GUI app behavior every release (see https://stackoverflow.com/q/135688/483528). As a
+# workaround to allow GitHub Desktop to work, add this (hopefully harmless) setting here.
+export PATH=$PATH:/usr/local/bin
+
+# Disable output not usually helpful when running in automation (such as guidance to run plan after init)
+export TF_IN_AUTOMATION=1
+
+# Store and return last failure from validate so this can validate every directory passed before exiting
+VALIDATE_ERROR=0
+
+for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
+  echo "--> Running 'tofu validate' in directory '$dir'"
+  pushd "$dir" >/dev/null
+  tofu init -backend=false || VALIDATE_ERROR=$?
+  tofu validate || VALIDATE_ERROR=$?
+  popd >/dev/null
+done
+
+exit ${VALIDATE_ERROR}
