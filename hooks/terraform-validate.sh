a53f1d18d719001413ac9747e9ad2827d1558f1a
Gabor <gesperon@mac.com>
2025-07-11 15:19:29 +0200

    custom dir for terraform validate

--- hooks/terraform-validate.sh
+++ hooks/terraform-validate.sh
@@ -16,6 +16,7 @@ VALIDATE_ERROR=0
 for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
   echo "--> Running 'terraform validate' in directory '$dir'"
   pushd "$dir" >/dev/null
+  TF_DATA_DIR=${dir}/.terraform-validate
   [[ $(shopt -s nullglob; echo *.tofu) ]] && tf_bin=tofu || tf_bin=terraform
   $tf_bin init -backend=false || VALIDATE_ERROR=$?
   $tf_bin validate || VALIDATE_ERROR=$?
